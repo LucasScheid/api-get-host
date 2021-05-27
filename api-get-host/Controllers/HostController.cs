91d7b6b92d77d6ad487e383c0a7f13881f7730da
Lucas Scheid <consultor.scheid@leomadeiras.com.br>
2021-05-27 10:33:14 -0300

    Add project files.

---
+++ api-get-host/Controllers/HostController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace api_get_host.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HostController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return string.Concat(Environment.MachineName);
+        }
+    }
+}
